0eaa9c1155574d11dded14dfdb94228059db5be0
Ankur <ankurgecr@gmail.com>
2018-10-20 13:49:58 +0530

    Minor UI

--- app/src/main/java/info/ankurpandya/phonenumberedittext/demo/PhoneNumberApp.java
+++ app/src/main/java/info/ankurpandya/phonenumberedittext/demo/PhoneNumberApp.java
@@ -1,13 +1,11 @@
 package info.ankurpandya.phonenumberedittext.demo;
 
 import android.app.Application;
-import android.helper.PhoneNumberEditText;
 
 public class PhoneNumberApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        PhoneNumberEditText.initCountries(this);
     }
 }
